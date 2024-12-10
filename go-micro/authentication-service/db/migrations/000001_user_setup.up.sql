ff598c7f8247308277824ef9971a7d6ce08c0251
a-kumar5 <ayush.1306.kumar@gmail.com>
2024-12-10 12:20:47 +0530

    auth service

--- go-micro/authentication-service/db/migrations/000001_user_setup.up.sql
+++ go-micro/authentication-service/db/migrations/000001_user_setup.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE "users" (
   "id" BIGSERIAL PRIMARY KEY,
   "email" varchar(256) UNIQUE NOT NULL,
   "hased_password" varchar(256) NOT NULL,
+  "active" varchar(256) NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now()),
   "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
