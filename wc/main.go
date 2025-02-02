d7bcf219dfac94ba794b86a284594b1f668b8da1
a-kumar5 <ayush.1306.kumar@gmail.com>
2025-02-02 16:49:34 +0530

    Feat: Flag Support added 1. l flag just to get line count 2. w flag just to get word count 3. c flag just to get character count

--- wc/main.go
+++ wc/main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
+/*
+func getAllFlags(args []string) []string {
+	if len(args) > 1{
+
+	}
+}
+*/
+
 func main() {
 	//This is go implementation for unix wc
+
+	// get flags from the users.
+	var printLineCount, printWordCount, printCharCount bool
+	flag.BoolVar(&printLineCount, "l", false, "to only get line count")
+	flag.BoolVar(&printWordCount, "w", false, "to only get word count")
+	flag.BoolVar(&printCharCount, "c", false, "to only get character count")
+	flag.Parse()
+	fnames := flag.CommandLine.Args()
+
+	// variables defined to store total line count, word count and character count
 	var tlc, twc, tcc int
-	for _, fname := range os.Args[1:] {
+
+	for _, fname := range fnames {
 		var lc, wc, cc int
 		file, err := os.Open(fname)
 		if err != nil {
@@ -26,10 +46,29 @@ func main() {
 		tlc += lc
 		twc += wc
 		tcc += cc
-		fmt.Printf("%7d %7d %7d %s\n", lc, wc, cc, fname)
-		file.Close()
+		if printLineCount {
+			fmt.Printf("%7d %s\n", lc, fname)
+			file.Close()
+		} else if printWordCount {
+			fmt.Printf("%7d %s\n", wc, fname)
+			file.Close()
+		} else if printCharCount {
+			fmt.Printf("%7d %s\n", cc, fname)
+			file.Close()
+		} else {
+			fmt.Printf("%7d %7d %7d %s\n", lc, wc, cc, fname)
+			file.Close()
+		}
 	}
-	if len(os.Args) > 2 {
-		fmt.Printf("%7d %7d %7d %s\n", tlc, twc, tcc, "total")
+	if len(fnames) > 1 {
+		if printLineCount {
+			fmt.Printf("%7d %s\n", tlc, "total")
+		} else if printCharCount {
+			fmt.Printf("%7d %s\n", tcc, "total")
+		} else if printWordCount {
+			fmt.Printf("%7d %s\n", twc, "total")
+		} else {
+			fmt.Printf("%7d %7d %7d %s\n", tlc, twc, tcc, "total")
+		}
 	}
 }
