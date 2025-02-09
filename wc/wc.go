3cc8e50e8c323cf3aab6c2920183f495c1f055b5
a-kumar5 <ayush.1306.kumar@gmail.com>
2025-02-09 13:00:37 +0530

    Code resuability using print metadata function

--- wc/wc.go
+++ wc/wc.go
@@ -8,68 +8,71 @@ import (
 	"strings"
 )
 
-/*
-func getAllFlags(args []string) []string {
-	if len(args) > 1{
-
-	}
-}
-*/
-
 type fMetadata struct {
-	fname     string
-	LineCount int
-	WordCount int
-	CharCount int
+	lines int
+	words int
+	chars int
 }
 
-func getFileMetadata(fname string) fMetadata {
-	var fm fMetadata
+func getFileMetadata(fname string) (fMetadata, error) {
 	file, err := os.Open(fname)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		return fMetadata{}, err
 	}
-	fm.fname = fname
+	defer file.Close()
+
+	var meta fMetadata
 	scan := bufio.NewScanner(file)
 	for scan.Scan() {
 		s := scan.Text()
-		fm.WordCount += len(strings.Fields(s))
-		fm.CharCount += len(s)
-		fm.LineCount++
+		meta.words += len(strings.Fields(s))
+		meta.chars += len(s)
+		meta.lines++
 	}
-	return fm
+	return meta, scan.Err()
+}
+
+func printMetadata(fname string, meta fMetadata, flags map[string]bool) {
+	switch {
+	case flags["l"]:
+		fmt.Printf("%7d %s\n", meta.lines, fname)
+	case flags["w"]:
+		fmt.Printf("%7d %s\n", meta.words, fname)
+	case flags["c"]:
+		fmt.Printf("%7d %s\n", meta.chars, fname)
+	default:
+		fmt.Printf("%7d %7d %7d %s\n", meta.lines, meta.words, meta.chars, fname)
+	}
 }
 
 func main() {
-	//This is go implementation for unix wc
-
-	// get flags from the users.
-	var printLineCount, printWordCount, printCharCount bool
-	flag.BoolVar(&printLineCount, "l", false, "to only get line count")
-	flag.BoolVar(&printWordCount, "w", false, "to only get word count")
-	flag.BoolVar(&printCharCount, "c", false, "to only get character count")
+	var l, w, c bool
+	flag.BoolVar(&l, "l", false, "line count")
+	flag.BoolVar(&w, "w", false, "word count")
+	flag.BoolVar(&c, "c", false, "character count")
 	flag.Parse()
 	fnames := flag.CommandLine.Args()
+
+	flags := map[string]bool{
+		"l": l,
+		"w": w,
+		"c": c,
+	}
 
 	// variables defined to store total line count, word count and character count
 	var total fMetadata
 	for _, fname := range fnames {
-		fm := getFileMetadata(fname)
-		fmt.Printf("%7d %7d %7d %s\n", fm.LineCount, fm.WordCount, fm.CharCount, fm.fname)
-		total.fname = "total"
-		total.LineCount += fm.LineCount
-		total.WordCount += fm.WordCount
-		total.CharCount += fm.CharCount
+		meta, err := getFileMetadata(fname)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error:", err)
+			continue
+		}
+		printMetadata(fname, meta, flags)
+		total.lines += meta.lines
+		total.words += meta.words
+		total.chars += meta.chars
 	}
 	if len(fnames) > 1 {
-		if printLineCount {
-			fmt.Printf("%7d %s\n", total.LineCount, "total")
-		} else if printCharCount {
-			fmt.Printf("%7d %s\n", total.CharCount, "total")
-		} else if printWordCount {
-			fmt.Printf("%7d %s\n", total.WordCount, "total")
-		} else {
-			fmt.Printf("%7d %7d %7d %s\n", total.LineCount, total.WordCount, total.CharCount, "total")
-		}
+		printMetadata("total", total, flags)
 	}
 }
