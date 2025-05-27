00526f008494613a362f9c3961c512fe5a67054b
a-kumar5 <ayush.1306.kumar@gmail.com>
2025-05-27 12:19:49 +0530

    Added goroutine code

---
+++ matt-course/goroutine/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type result struct {
+	url     string
+	err     error
+	latency time.Duration
+}
+
+func get(url string, ch chan<- result) {
+	start := time.Now()
+
+	if resp, err := http.Get(url); err != nil {
+		ch <- result{url, err, 0}
+	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t}
+		resp.Body.Close()
+	}
+}
+
+var nextID = make(chan int)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>You got %d</h1>", <-nextID)
+}
+
+func counter() {
+	for i := 0; ; i++ {
+		nextID <- i
+	}
+}
+
+func main() {
+	go counter()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	/*
+	   results := make(chan result)
+
+	   	list := []string{
+	   		"https://amazon.com",
+	   		"https://google.com",
+	   		"https://facebook.com",
+	   		"https://x.com",
+	   	}
+
+	   	for _, url := range list {
+	   		go get(url, results)
+	   	}
+
+	   	for range list {
+	   		r := <-results
+	   		if r.err != nil {
+	   			log.Printf("%-20s %s\n", r.url, r.err)
+	   		} else {
+	   			log.Printf("%-20s %s\n", r.url, r.latency)
+	   		}
+	   	}
+	*/
+}
