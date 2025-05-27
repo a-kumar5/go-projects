c88b4af3d8d01f50d952a1c2c8fcddc19845cf5f
a-kumar5 <ayush.1306.kumar@gmail.com>
2025-05-27 13:50:18 +0530

    Prime number calculation

--- matt-course/goroutine/main.go
+++ matt-course/goroutine/main.go
@@ -37,23 +37,38 @@ func counter(ch chan<- int) {
 }
 
 func generate(limit int, ch chan<- int) {
-	for i := 1; i <= limit; i++ {
+	for i := 2; i <= limit; i++ {
 		ch <- i
 	}
 	close(ch)
 }
 
-func main() {
+func filter(src <-chan int, dst chan<- int, prime int) {
+	for i := range src {
+		if i%prime != 0 {
+			dst <- i
+		}
+	}
+	close(dst)
+}
+
+func sieve(limit int) {
 	ch := make(chan int)
-	go generate(100, ch)
+	go generate(limit, ch)
 	for {
 		prime, ok := <-ch
 		if !ok {
 			break
 		}
+		ch1 := make(chan int)
+		go filter(ch, ch1, prime)
+		ch = ch1
 		fmt.Print(prime, " ")
 	}
+}
 
+func main() {
+	sieve(100)
 	/*
 		var nextID nextCh = make(chan int)
 		go counter(nextID)
