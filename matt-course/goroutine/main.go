8ed8317972e8db9465c69c4ec2f993251d0aaf60
a-kumar5 <ayush.1306.kumar@gmail.com>
2025-05-27 12:25:37 +0530

    move nextID from global to local variable

--- matt-course/goroutine/main.go
+++ matt-course/goroutine/main.go
@@ -25,21 +25,22 @@ func get(url string, ch chan<- result) {
 	}
 }
 
-var nextID = make(chan int)
+type nextCh chan int
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>You got %d</h1>", <-nextID)
+func (ch nextCh) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>You got %d</h1>", <-ch)
 }
 
-func counter() {
+func counter(ch chan<- int) {
 	for i := 0; ; i++ {
-		nextID <- i
+		ch <- i
 	}
 }
 
 func main() {
-	go counter()
-	http.HandleFunc("/", handler)
+	var nextID nextCh = make(chan int)
+	go counter(nextID)
+	http.HandleFunc("/", nextID.handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	/*
